9aad3176e73863cc12d5164b2842b619e9eae86e
moritzgoeckel <moritz.goeckel@gmail.com>
2018-02-15 13:29:20 +0100

    Implemented and tested hashmap

---
+++ src/java/Implementations/HashMap.java
@@ -0,0 +1,114 @@
+package Implementations;
+
+public class HashMap<K, V> {
+
+    private LinkedList<KeyValuePair<K, V>>[] array = new LinkedList[3];
+    private int elementsCount = 0;
+
+    public HashMap(){
+        for(int i = 0; i < array.length; i++)
+            array[i] = new LinkedList<>();
+
+        setArraySize(3);
+    }
+
+    private void setArraySize(int size){
+        LinkedList<KeyValuePair<K, V>> newArray[] = new LinkedList[size];
+        for(int i = 0; i < size; i++)
+            newArray[i] = new LinkedList<>();
+
+        for(LinkedList<KeyValuePair<K, V>> bucket : array)
+            for(int i = 0; i < bucket.size(); i++) //Todo: Inefficient, should use an iterator
+                newArray[bucket.get(i).key.hashCode() % size].add(bucket.get(i));
+
+        array = newArray;
+    }
+
+    public void set(K key, V value){
+        KeyValuePair<K, V> pair = new KeyValuePair<>(key, value);
+
+        int bucket = key.hashCode() % array.length;
+        int listIndex = array[bucket].getIndex(pair);
+
+        if(listIndex == -1) {
+            array[bucket].add(pair);
+            elementsCount++;
+        }
+        else
+            array[bucket].get(listIndex).value = pair.value;
+
+        if(elementsCount > array.length)
+            setArraySize(array.length + array.length / 3);
+    }
+
+    public V get(K key){
+        KeyValuePair<K, V> pair = new KeyValuePair<>(key, null);
+
+        int bucket = key.hashCode() % array.length;
+        int listIndex = array[bucket].getIndex(pair);
+
+        if(listIndex == -1)
+            throw new RuntimeException("Key not found: " + key);
+
+        return array[bucket].get(listIndex).value;
+    }
+
+    public void remove(K key){
+        KeyValuePair<K, V> pair = new KeyValuePair<>(key, null);
+
+        int bucket = key.hashCode() % array.length;
+        int listIndex = array[bucket].getIndex(pair);
+
+        if(listIndex == -1)
+            throw new RuntimeException("Key not found: " + key);
+
+        array[bucket].remove(listIndex);
+        elementsCount--;
+
+        if(elementsCount < array.length - array.length / 3 && elementsCount > 3)
+            setArraySize(elementsCount);
+    }
+
+    public boolean contains(K key){
+        KeyValuePair<K, V> pair = new KeyValuePair<>(key, null);
+
+        int bucket = key.hashCode() % array.length;
+        return array[bucket].getIndex(pair) != -1;
+    }
+
+    public int size(){
+        return elementsCount;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        for(int i = 0; i < array.length; i++) {
+            sb.append(array[i].toString());
+            if (i < array.length - 1)
+                sb.append(", ");
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
+
+class KeyValuePair<K, V>{
+    public K key;
+    public V value;
+
+    public KeyValuePair(K key, V value){
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return key.equals(((KeyValuePair<K, V>) obj).key);
+    }
+
+    @Override
+    public String toString() {
+        return "{"+ key + ": " + value + "}";
+    }
+}
---
+++ src/java/Tests/HashMapTest.java
@@ -0,0 +1,122 @@
+package Tests;
+
+import Implementations.HashMap;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HashMapTest {
+    @Test
+    public void testSetOne(){
+        HashMap<String, String> map = new HashMap<>();
+        map.set("Hallo", "Welt");
+
+        assertEquals("[[], [{Hallo: Welt}], []]", map.toString());
+    }
+
+    @Test
+    public void testSetThree(){
+        HashMap<String, String> map = new HashMap<>();
+        map.set("A", "B");
+        map.set("C", "D");
+        map.set("E", "F");
+
+        assertEquals("[[{E: F}], [{C: D}], [{A: B}]]", map.toString());
+    }
+
+    @Test
+    public void testSetMany(){
+        HashMap<String, String> map = new HashMap<>();
+        char c[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".toCharArray();
+        for(int i = 0; i < c.length - 1; i++)
+            map.set(Character.toString(c[i]), Character.toString(c[i + 1]));
+
+        //Check if they are all in the map
+        for(int i = 0; i < c.length - 1; i++)
+            assertTrue(map.toString().contains("{" + Character.toString(c[i]) + ": " + Character.toString(c[i + 1]) + "}"),
+                    "Cant find added item in toString: " + Character.toString(c[i]));
+    }
+
+    @Test
+    public void testGet(){
+        HashMap<String, String> map = new HashMap<>();
+        char c[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".toCharArray();
+
+        //Adding
+        for(int i = 0; i < c.length - 1; i++)
+            map.set(Character.toString(c[i]), Character.toString(c[i + 1]));
+
+        //Checking
+        for(int i = 0; i < c.length - 1; i++)
+            assertEquals(Character.toString(c[i + 1]), map.get(Character.toString(c[i])));
+    }
+
+    @Test
+    public void testOverrideValue(){
+        HashMap<String, String> map = new HashMap<>();
+        map.set("A", "B");
+        assertTrue(map.toString().contains("{A: B}"), "This should be ensured by testSetOne already");
+
+        map.set("A", "C");
+        assertTrue(map.toString().contains("{A: C}"), "New value should be in the array");
+        assertFalse(map.toString().contains("{A: B}"), "Old value should not exist anymore");
+    }
+
+    @Test
+    public void testRemove(){
+        HashMap<String, String> map = new HashMap<>();
+        map.set("A", "B");
+        map.set("D", "E");
+
+        map.remove("A");
+
+        assertTrue(map.toString().contains("{D: E}"), "Unaffected value still there");
+        assertFalse(map.toString().contains("{A: B}"), "Deleted value should be gone");
+    }
+
+    @Test
+    public void testSizeAdding(){
+        HashMap<String, String> map = new HashMap<>();
+        char c[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".toCharArray();
+        for(int i = 0; i < c.length - 1; i++) {
+            assertEquals(i, map.size());
+            map.set(Character.toString(c[i]), Character.toString(c[i + 1]));
+        }
+    }
+
+    @Test
+    public void testSizeRemoving(){
+        HashMap<String, String> map = new HashMap<>();
+        char c[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".toCharArray();
+        for(int i = 0; i < c.length - 1; i++)
+            map.set(Character.toString(c[i]), Character.toString(c[i + 1]));
+
+        int size = map.size();
+        for(int i = 0; i < c.length - 1; i++) {
+            map.remove(Character.toString(c[i]));
+            assertEquals(--size, map.size());
+        }
+    }
+
+    @Test
+    public void testContains(){
+        HashMap<String, String> map = new HashMap<>();
+        char c[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".toCharArray();
+
+        //Adding
+        for(int i = 0; i < c.length - 1; i++) {
+            assertFalse(map.contains(Character.toString(c[i])));
+            map.set(Character.toString(c[i]), Character.toString(c[i + 1]));
+            assertTrue(map.contains(Character.toString(c[i])));
+        }
+
+        //Removing
+        for(int i = 0; i < c.length - 1; i++) {
+            assertTrue(map.contains(Character.toString(c[i])));
+            map.remove(Character.toString(c[i]));
+            assertFalse(map.contains(Character.toString(c[i])));
+        }
+    }
+}
